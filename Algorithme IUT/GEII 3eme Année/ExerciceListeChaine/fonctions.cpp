00102585bc377772a76d89f31a60ff7850c14974
IAidenI <133039461+IAidenI@users.noreply.github.com>
2023-10-18 16:13:49 +0200

    Final Application

--- Algorithme IUT/GEII 3eme Année/ExerciceListeChaine/fonctions.cpp
+++ Algorithme IUT/GEII 3eme Année/ExerciceListeChaine/fonctions.cpp
@@ -1,17 +1,11 @@
 #include "fonctions.h"
 
-void Menu(string fileName) {
+void Menu(const string& fileName) {
     /* ==== AFFICHAGE DU MENU ==== */
-    Node* liste = NULL;
+    Node* liste = nullptr;
     string choix;
-    cout << "Veillez saisir l'un des choix disponible." << endl;
-    cout << "[enter] - Saisir des coupes de points à stocker" << endl;
-    cout << "[print] - Affiche les points stocké" << endl;
-    cout << "[rfile] - Affiche le contenu du fichier dans une liste chaînée" << endl;
-    cout << "[wfile] - Ecris le contenu de la liste chaînée dans le fichier" << endl;
-    cout << "[help] - Affiche ce menu" << endl;
-    cout << "[exit] - Sort de l'application" << endl;
 
+    Clear();
     // Récupère le choix de l'utilisateur
     while (true) {
         cout << ">";
@@ -20,7 +14,7 @@ void Menu(string fileName) {
 
         // Met la saisie de l'utilisateur en lowerCase
         for (unsigned int i = 0; choix[i] != '\0'; i++) {
-            choix[i] = tolower(choix[i]);
+            choix[i] = static_cast<char>(tolower(choix[i]));
         }
 
         if (choix == "enter") {
@@ -33,18 +27,21 @@ void Menu(string fileName) {
             WriteFile(SelectFile(fileName), liste);
         } else if (choix == "help") {
             cout << "=== HELP MENU ===" << endl;
-            cout << "[enter] - Saisir des coupes de points à stocker" << endl;
-            cout << "[print] - Affiche les points stocké" << endl;
-            cout << "[rfile] - Affiche le contenu du fichier" << endl;
-            cout << "[wfile] - Ecris dans le fichier" << endl;
-            cout << "[help] - Affiche ce menu" << endl;
-            cout << "[exit] - Sort de l'application" << endl;
+            Help();
+        } else if (choix == "search") {
+            Search(SelectFile(fileName), liste);
+        } else if (choix == "sort") {
+            SortAbs(&liste);
+        } else if (choix == "graph") {
+            Graph(liste);
+        } else if (choix == "clear") {
+            Clear();
         } else if (choix == "exit") {
             Delete(liste);
             cout << "Au revoir." << endl;
             break;
         } else {
-            cout << "[!] Erreur : Erreur de lors de la saisie, saisire 'help' pour plus d'informations." << endl;
+            cout << "[!] Erreur : Erreur de lors de la saisie, 'help' pour plus d'informations." << endl;
         }
     }
 }
@@ -69,7 +66,7 @@ void Saisie(Node** liste) {
         } else {
             // Met la saisie de l'utilisateur en lowerCase
             for (unsigned int i = 0; choix[i] != '\0'; i++) {
-                choix[i] = tolower(choix[i]);
+                choix[i] = static_cast<char>(tolower(choix[i]));
             }
 
             if (choix == "done") {
@@ -109,7 +106,7 @@ void Delete(Node* liste) {
         delete toDel; // Supprime le nœud précédent
     }
 
-    cout << "[+] Liste supprimé avec succès." << endl;
+    cout << "[+] Liste supprimée avec succès." << endl;
 }
 
 void PrintList(Node* liste) {
@@ -118,7 +115,7 @@ void PrintList(Node* liste) {
         return;
     }
 
-    cout << "Contenue de la liste chaîné :" << endl;
+    cout << "Contenu de la liste chaîné :" << endl;
 
     // Parcours la liste et affiche chaque élément sous le format (x;y) -> (x;y)
     while (liste != nullptr) {
@@ -131,25 +128,20 @@ void PrintList(Node* liste) {
     cout << endl;
 }
 
-string SelectFile(string fileName) {
-    if (fileName == "") {
+string SelectFile(const string& fileName) {
+    if (fileName.empty()) {
         string choix_fileName;
-        cout << "Selectionner le chemin absolu d'un fichier." << endl << ">";
+        cout << "Selectionner le chemin absolu d'un fichier." << endl;
+        cout << ">";
         cin >> choix_fileName;
-
-        // Vérifie que le fichier existe
-        ifstream file(choix_fileName);
-        if (!file.is_open()) {
-            cout << "[!] Erreur : Le fichier seléctionné est introuvable." << endl;
-            return "";
-        }
+        cin.ignore();
         return choix_fileName;
     } else {
         return fileName;
     }
 }
 
-void ReadFile(string fileName, Node** liste) {
+void ReadFile(const string& fileName, Node** liste) {
     float x = 0;
     float y = 0;
     string ligne;
@@ -177,20 +169,216 @@ void ReadFile(string fileName, Node** liste) {
     file.close();
 }
 
-void WriteFile(string fileName, Node* liste) {
+void WriteFile(const string& fileName, Node* liste) {
     // Ouvre le fichier en mode écriture en vidant son contenu
-    ofstream file(fileName, ios::trunc);
+    ifstream file_check(fileName);
 
-    if (!file.is_open()) {
-        cout << "[!] Erreur : Impossible d'ouvrir le fichier." << endl;
+    if (!file_check.good()) {
+        string choix;
+        cout << "[!] Erreur : Le fichier seléctionné est introuvable." << endl;
+        cout << "Voulez vous crée le fichier (yes/no) ?" << endl;
+        cout << ">";
+        cin >> choix;
+
+        // Met la saisie de l'utilisateur en lowerCase
+        for (unsigned int i = 0; choix[i] != '\0'; i++) {
+            choix[i] = static_cast<char>(tolower(choix[i]));
+        }
+
+        if (choix == "yes" || choix == "y") {
+            file_check.close();
+            ofstream file(fileName, ios::trunc);
+
+            // Place tout le contenu de la liste chaîné dans le fichier
+            while (liste != nullptr) {
+                file << liste->element.x << " " << liste->element.y << endl;
+                liste = liste->psuivant;
+            }
+
+            cout << "[+] Contenu de la liste chaînée stocké dans " << fileName << endl;
+
+            file.close();
+        } else if (choix == "no" || choix == "n") {
+            file_check.close();
+            cout << "[-] Sortie." << endl;
+            return;
+        }
+    } else {
+        ofstream file(fileName, ios::trunc);
+
+        // Place tout le contenu de la liste chaîné dans le fichier
+        while (liste != nullptr) {
+            file << liste->element.x << " " << liste->element.y << endl;
+            liste = liste->psuivant;
+        }
+
+        cout << "[+] Contenu de la liste chaînée stocké dans " << fileName << endl;
+        file_check.close();
+    }
+}
+
+
+void Search(const string& fileName, Node* liste) {
+    float x_raw = 0;
+    float y_raw = 0;
+    string choix;
+    cout << "Saisir le couple de point à vérifier (pour entrer 10 et 23 saisir 10 23) : " << endl;
+    cout << ">";
+    getline(cin, choix); // Permet de lire la ligne complète à partir de la sortie standand. Pour pas que si l'utilisateur saisisse 10 23 le programme récupère juste 10
+
+    /*
+    Vu que l'utilisateur doit saisir par exempl 10 23, pour le stocker
+    dans deux variables x = 10 et y = 23 on se sert de istringstream
+     */
+
+    istringstream iss(choix); // Crée un flux d'entrée à partir de la chaîne choix.
+    // On extrait les données dans x et y. '>>' lit les données jusqu'à ce qu'il atteigne un espace ou un '\n'
+    if (iss >> x_raw >> y_raw) {
+        bool check = false;
+        float x = 0;
+        float y = 0;
+        string ligne;
+        // Ouvre un fichier en mode lecture
+        ifstream file(fileName);
+
+        if (!file.is_open()) {
+            cout << "[!] Erreur : Impossible d'ouvrir le fichier." << endl;
+            return;
+        }
+
+        // Place le contenu du fichier dans la liste chaîné
+        while (getline(file, ligne)) {
+            // Split 10 23 en x = 10 et y = 23
+            istringstream iss(ligne);
+            if (iss >> x >> y) {
+                if (x == x_raw && y == y_raw) {
+                    cout << "[*] Le couple de point existe dans le fichier : " << fileName << endl;
+                    check = true;
+                    break;
+                } else {
+                    check = false;
+                }
+            } else {
+                cout << "[!] Erreur : La ligne contenant : " << ligne << " n'est pas valide." << endl;
+            }
+        }
+        file.close();
+
+        if (!check) {
+            cout << "[-] Le point n'existe pas dans le fichier." << endl;
+        }
+
+        if (liste == nullptr) {
+            cout << "[!] La liste est vide." << endl;
+            return;
+        }
+
+        // Parcours la liste et affiche chaque élément sous le format (x;y) -> (x;y)
+        while (liste != nullptr) {
+            if (liste->element.x == x_raw && liste->element.y == y_raw) {
+                cout << "[*] Le couple de point existe dans la liste chaîné." << endl;
+                check = true;
+                break;
+            } else {
+                check = false;
+            }
+            liste = liste->psuivant; // Passe au nœud suivant dans la liste
+        }
+
+        if (!check) {
+            cout << "[-] Le point est introuvable dans la liste." << endl;
+        }
+    } else {
+        cout << "[!] Erreur lors de la saisie." << endl;
+    }
+}
+
+
+void SortAbs(struct Node** head) {
+    // Vérifier si la liste est vide ou ne contient qu'un seul élément
+    if (*head == nullptr || (*head)->psuivant == nullptr) {
+        cout << "[!] Erreur : Liste vide ou avec un seul élément." << endl;
         return;
     }
 
-    // Place tout le contenu de la liste chaîné dans le fichier
-    while (liste != nullptr) {
-        file << liste->element.x << " " << liste->element.y << endl;
-        liste = liste->psuivant;
+    struct Node* sort = nullptr;
+
+    // Parcourir la liste d'origine
+    while (*head != nullptr) {
+        struct Node* actuel = *head;  // Pointeur vers le nœud actuel
+        *head = (*head)->psuivant;    // Avancer dans la liste d'origine
+
+        // Insérer le noeud actuel dans la liste triée en fonction de la valeur x
+        if (sort == nullptr || actuel->element.x < sort->element.x) {
+            actuel->psuivant = sort;
+            sort = actuel;
+        } else {
+            // Parcourir la liste triée pour trouver la position d'insertion
+            struct Node* temp = sort;
+            while (temp->psuivant != nullptr && temp->psuivant->element.x < actuel->element.x) {
+                temp = temp->psuivant;
+            }
+            actuel->psuivant = temp->psuivant;
+            temp->psuivant = actuel;
+        }
     }
 
-    cout << "[+] Contenu de la liste chaînée stocké dans " << fileName << endl;
+    *head = sort;
+    cout << "[+] Liste triée. (Saisir 'print' pour afficher la liste)" << endl;
+}
+
+
+void Graph(struct Node* liste) {
+    if (liste == nullptr) {
+        cout << "[!] Erreur : Liste vide." << endl;
+        return;
+    }
+
+    ofstream gnuplotPipe;
+    gnuplotPipe.open("gnuplot_commands.txt");
+
+    if (gnuplotPipe.is_open()) {
+        gnuplotPipe << "plot '-' with linespoints title 'Graphique de la liste chaînée'\n";
+
+        // Parcourez la liste pour extraire les points et les ajouter à la commande de tracé
+        while (liste != nullptr) {
+            gnuplotPipe << liste->element.x << " " << liste->element.y << "\n";
+            liste = liste->psuivant;
+        }
+
+        gnuplotPipe << "e\n"; // Terminer la saisie des données
+
+        gnuplotPipe << "pause mouse key\n"; // Mettez en pause pour afficher le tracé
+        gnuplotPipe.close();
+
+        system("gnuplot < gnuplot_commands.txt");
+    } else {
+        cout << "[!] Erreur : Création du fichier de commandes Gnuplot échoué." << endl;
+    }
+}
+
+
+void Help() {
+    cout << "Veillez saisir l'un des choix disponible." << endl;
+    cout << "\t[enter]  - Saisir des couples de points à stocker" << endl;
+    cout << "\t[print]  - Affiche les points stocké" << endl;
+    cout << "\t[rfile]  - Affiche le contenu du fichier dans une liste chaînée" << endl;
+    cout << "\t[wfile]  - Ecris le contenu de la liste chaînée dans le fichier" << endl;
+    cout << "\t[search] - Cherche si le point existe" << endl;
+    cout << "\t[sort]   - Trier par ordre croissant les abscisses" << endl;
+    cout << "\t[graph]  - Affichage graphique" << endl;
+    cout << "\t[clear]  - Efface l'affichage" << endl;
+    cout << "\t[help]   - Affiche ce menu" << endl;
+    cout << "\t[exit]   - Sort de l'application" << endl;
+}
+
+
+void Clear() {
+#ifdef _WIN32 // Si c'est windows
+    system("cls");
+    Help();
+#else // Si c'est UNIX
+    system("clear");
+    Help();
+#endif
 }
--- Algorithme IUT/GEII 3eme Année/ExerciceListeChaine/fonctions.h
+++ Algorithme IUT/GEII 3eme Année/ExerciceListeChaine/fonctions.h
@@ -25,13 +25,13 @@ Entrée/Sortie : Vide
 Sortie : Vide
 Traitement : Affiche le Menu
 */
-void Menu(string fileName);
+void Menu(const string& fileName);
 
 
 /*
 Nom : Saisie
-Entrée : Un pointeur de pointeur vers une liste
-Entrée/Sortie : Vide
+Entrée : Vide
+Entrée/Sortie : Un pointeur de pointeur vers une liste
 Sortie : Vide
 Traitement : Permet d'attendre que l'utilisateur saisie ses données et le stock dans la liste chaîné
 */
@@ -40,8 +40,8 @@ void Saisie(Node** liste);
 
 /*
 Nom : Insert
-Entrée : Un pointeur de pointeur vers la liste chaîné et le point à ajouter à cette liste
-Entrée/Sortie : Vide
+Entrée : Le point à ajouter à cette liste
+Entrée/Sortie : Un pointeur de pointeur vers la liste chaîné
 Sortie : Vide
 Traitement : Permet d'ajouter un élement à la liste chaîné
 */
@@ -50,8 +50,8 @@ void Insert(Node** liste, point newData);
 
 /*
 Nom : Delete
-Entrée : Pointeur de la liste chaîné à supprimer
-Entrée/Sortie : Vide
+Entrée : Vide
+Entrée/Sortie : Pointeur de la liste chaîné à supprimer
 Sortie : Vide
 Traitement : Permet de suprimer tout les éléments de la liste chaîné
 */
@@ -60,8 +60,8 @@ void Delete(Node* liste);
 
 /*
 Nom : PrintList
-Entrée : Pointeur de la liste chaîné à afficher
-Entrée/Sortie : Vide
+Entrée : Vide
+Entrée/Sortie : Pointeur de la liste chaîné à afficher
 Sortie : Vide
 Traitement : Permet d'afficher la liste chaîné
 */
@@ -70,29 +70,79 @@ void PrintList(Node* liste);
 
 /*
 Nom : SelectFile
-Entrée : Un string qui va contenir le chemin du fichier qsi l'utiliseur l'as rentré au démarage
+Entrée : Un string qui va contenir le chemin du fichier si l'utiliseur l'as rentré au démarage
 Entrée/Sortie : Vide
 Sortie : Un string qui correspond au chemin d'accès absolu du fichier
 Traitement : Récupère le chemin du fichier
 */
-string SelectFile(string fileName);
+string SelectFile(const string& fileName);
 
 
 /*
 Nom : ReadFile
-Entrée : Un string le chemin d'accès du fichier et un pointeur de pointeur pour stocker les données dans la liste chaînée
-Entrée/Sortie : Vide
+Entrée : Un string le chemin d'accès du fichier
+Entrée/Sortie : Un pointeur de pointeur pour lire les données dans la liste chaînée
 Sortie : Vide
 Traitement : Lis le contenu du fichier et le stock dans la liste chaînée
 */
-void ReadFile(string fileName, Node** liste);
+void ReadFile(const string& fileName, Node** liste);
 
 
 /*
 Nom : WriteFile
-Entrée : Vide
-Entrée/Sortie : Vide
+Entrée : Un string le chemin d'accès du fichier
+Entrée/Sortie : Un pointeur de pointeur pour stocker les données dans la liste chaînée
 Sortie : Vide
 Traitement : Ecris le contenu de la liste chaînée dans le fichier
 */
-void WriteFile(string fileName, Node* liste);
+void WriteFile(const string& fileName, Node* liste);
+
+
+/*
+Nom : Search
+Entrée : Un string le chemin d'accès du fichier
+Entrée/Sortie : Un pointeur de pointeur pour stocker les données dans la liste chaînée
+Sortie : Vide
+Traitement : Cherche dans la liste chaîné et dans le fichier si le couple de point est présent
+*/
+void Search(const string& fileName, Node* liste);
+
+
+/*
+Nom : SortAbs
+Entrée : Vide
+Entrée/Sortie : Un pointeur vers un pointeur vers la liste chaînée, représentant la tête de la liste chaînée à trier.
+Sortie : Vide
+Traitement : Trie la liste chaînée par ordre croissant des abscisses
+*/
+void SortAbs(struct Node** head);
+
+
+/*
+Nom : Graph
+Entrée : Vide
+Entrée/Sortie : Un pointeur vers un pointeur vers la liste chaînée, représentant la tête de la liste chaînée à trier.
+Sortie : Vide
+Traitement : Trie la liste chaînée par ordre croissant des abscisses
+*/
+void Graph(struct Node* liste);
+
+
+/*
+Nom : Help
+Entrée : Vide
+Entrée/Sortie : Vide
+Sortie : Vide
+Traitement : Affiche le menu d'aide
+*/
+void Help();
+
+
+/*
+Nom : Clear
+Entrée : Vide
+Entrée/Sortie : Vide
+Sortie : Vide
+Traitement : Efface l'écran
+*/
+void Clear();
